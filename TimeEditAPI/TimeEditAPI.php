450c065aeab2e2a2ef9426af87bb873cf05bbf01
Martin <martin_mine@hotmail.com>
2014-02-06 16:46:44 +0100

    Test
    Longer description of test

--- TimeEditAPI/TimeEditAPI.php
+++ TimeEditAPI/TimeEditAPI.php
@@ -4,5 +4,6 @@ require_once('TimeEditAPIController.class.php');
 $controller = new TimeEditAPIController('https://web.timeedit.se/hig_no/db1/timeedit/p/open/r.csv?sid=3&h=t&p=0.minutes,2.months&objects=161569.182&ox=0&types=0&fe=0');
 $dom = $controller->getDOM();
 echo $dom->saveXML();
+echo "test";
 
 ?>
