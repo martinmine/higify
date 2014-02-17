ade5708a4e036fea0a2f92951e1226b478c852f1
Martin <martin_mine@hotmail.com>
2014-02-17 15:05:13 +0100

    All HTML prototypes now passes the W3C validator

--- TimeSchedule/time_schedule.js
+++ TimeSchedule/time_schedule.js
@@ -17,6 +17,33 @@ function setHeightAndWidth(xpathExpression, width, height)
 	}
 }
 
+var cellWidth;
+var cellHeight;
+
+function setTimeObjectLayout(width, height)
+{
+	cellWidth = width;
+	cellHeight = height;
+}
+
+function setTimeObject(id, offset, duration)
+{
+	var element = document.getElementById(id);
+
+	var offset = Math.floor(offset * (cellHeight / 60));
+	var length = Math.floor(duration * (cellHeight / 60));
+
+	if (length > cellHeight)	// If cross a border
+	{
+		length += Math.floor(length / cellHeight) * 3; // Add the size of the borders we crossed
+	}
+
+	element.style.height = length + "px";
+	element.style.width = cellWidth + "px";
+	element.style.marginTop = offset + "px";
+}
+
+/*
 function setTimeObjects(xpathExpression, width, height)
 {
 	var elements = runXpath(xpathExpression);
@@ -38,7 +65,7 @@ function setTimeObjects(xpathExpression, width, height)
 		element.style.width = width + "px";
 		element.style.marginTop = offset + "px";
 	}
-}
+}*/
 
 function setHeight(xpathExpression, height)
 {
@@ -59,26 +86,8 @@ function formatTimeTable(CELL_HEIGHT, CELL_WIDTH, CELL_DAY_HEADER_HEIGHT, CELL_D
 	setHeightAndWidth('//div[@class="dayHeader"]', CELL_WIDTH, CELL_DAY_HEADER_HEIGHT);
 	setHeightAndWidth('//div[@class="singleHour"]', CELL_WIDTH, CELL_HEIGHT);
 	
-	setTimeObjects('//div[contains(@class, "timeObjectContainer")]', CELL_WIDTH - 8, CELL_HEIGHT - 2);
+	//setTimeObjects('//div[contains(@class, "timeObjectContainer")]', CELL_WIDTH - 8, CELL_HEIGHT - 2);
 	setHeightAndWidth('//div[contains(@class, "dayPadding")]', CELL_WIDTH, CELL_DAY_PADDING_HEGIHT);
 	setHeightAndWidth('//div[contains(@class, "weekPadding")]', CELL_TIME_WIDTH - 4, CELL_DAY_PADDING_HEGIHT);
 }
 
-window.onload = function()
-{
-	try
-	{
-		wgxpath.install();	// Wicked-good-xpath: Ensure document.evaluate is working
-		var CELL_HEIGHT = 50;	// Height of all the cells in the time table
-		var CELL_WIDTH = 200;	// Width of all the cells expect the time identifiers
-		var CELL_DAY_HEADER_HEIGHT = 30;	// Height of the day labels
-		var CELL_DAY_PADDING_HEGIHT = 10;	// Height of the cell bellow the day headers
-		var CELL_TIME_WIDTH = 30;	// Width of all the other cells
-
-		formatTimeTable(CELL_HEIGHT, CELL_WIDTH, CELL_DAY_HEADER_HEIGHT, CELL_DAY_PADDING_HEGIHT, CELL_TIME_WIDTH);
-	}
-	catch (ex)
-	{
-		alert(ex.message);
-	}
-};
