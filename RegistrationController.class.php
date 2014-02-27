1a6d4b744de2b6fbba51b6a44993e244ff987d7c
Martin <martin_mine@hotmail.com>
2014-02-27 09:55:54 +0100

    Cleanup

--- RegistrationController.class.php
+++ RegistrationController.class.php
@@ -32,67 +32,55 @@ class RegistrationController implements IPageController
             $captcha = $_POST['recaptcha_response_field'];
             
             $captchaResponse = recaptcha_check_answer(HigifyConfig::RECAPTCHA_PRIVATE_KEY, $_SERVER['REMOTE_ADDR'], // Verify the security code
-                                       $_POST["recaptcha_challenge_field"], $_POST["recaptcha_response_field"]);
-            
-            $registrationFailure = false;
+                                       $_POST["recaptcha_challenge_field"], $captcha);
             
             if (strlen($username) == 0) // No username entered
             {
-                $registrationFailure = true;
                 $vals['ERROR_USR'] = new ErrorMessageView('Please enter a username');
             }
             else if (UserController::userExists($username)) // User already exists TODO: move userexists to UserController
             {
-                $registrationFailure = true;
                 $vals['ERROR_USR'] = new ErrorMessageView('A user with this username already exists. Try another one.');
                 $_POST['username'] = '';
             }
             
             if (strlen($password) == 0) // No password entered
             {
-                $registrationFailure = true;
                 $vals['ERROR_PSW'] = new ErrorMessageView('Please enter a password');
             }
             else if (in_array(strtolower($password), $mostUsedPasswords))
             {
-                $registrationFailure = true;
                 $vals['ERROR_PSW'] = new ErrorMessageView('You are not allowed to use the same password as the system administrator!');
             }
             else if (strlen($password) < 7) // Not secure enough password
             {
-                $registrationFailure = true;
                 $vals['ERROR_PSW'] = new ErrorMessageView('Enter a password longer than six characters');
             }
             
             if ($password != $passwordVerification) // Passwords didn't match
             {
-                $registrationFailure = true;
                 $vals['ERROR_PSWCONFIRM'] = new ErrorMessageView('The passwords you entered are not the same');
             }
             
             if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) // Invalid email
             {
-                $registrationFailure = true;
                 $vals['ERROR_EMAIL'] = new ErrorMessageView('Please specify a valid email address');
             }
-            else if (UserController::requestUserByEmail($email) !== NULL) // A user already exists with this email [TODO]
+            else if (UserController::requestUserByEmail($email) !== NULL) // A user already exists with this email
             {
-                $registrationFailure = true;
                 $vals['ERROR_EMAIL'] = new ErrorMessageView('An user with this email already exists');
             }
             
             if (strlen($captcha) == 0)
             {
-                $registrationFailure = true;
                 $vals['ERROR_CAPTCHA'] = new ErrorMessageView('Please enter the two words in the image above');
             }
             else if (!$captchaResponse->is_valid) // Invalid captcha
             {
-                $registrationFailure = true;
                 $vals['ERROR_CAPTCHA'] = new ErrorMessageView('Invalid security code');
             }
            
-            if ($registrationFailure == false)
+            if (!isset($vals['ERROR_CAPTCHA']))
             {
                 UserController::registerUser($username, $password, $email);
                 header('Location: login.php?registered');
