fb487a41a4c7aebd05df7482150d07e3a31c51d5
Martin <martin_mine@hotmail.com>
2014-03-04 13:50:09 +0100

    Cleanup

--- displayProfilePicture.php
+++ displayProfilePicture.php
@@ -1,26 +1,16 @@
 <?php
 require_once('SessionController.class.php');
 require_once('UserController.class.php');
+
+header("Content-type: image/jpeg");
+
+if (isset($_GET['id']))
+{
+	$picture = UserController::requestProfilePicture($_GET['id']);
 	
-	$profileID = (isset($_GET['id']))? $_GET['id']: NULL;
-	
-	if ($profileID !== NULL)
-	{
-		
-		//$userID = SessionController::requestLoggedinID();
-		$picture = UserController::requestProfilePicture($profileID);
-		
-		header("Content-type: image/jpeg");
-		if ($picture == NULL)
-		{
-			include('static/defaultpicture.png');
-		}
-		else
-			echo $picture;
-			
-	}
+	if ($picture === NULL)
+		include('static/defaultpicture.png');
 	else
-	{
-		header('Location: mainpage.php');
-	}
+		echo $picture;
+}
 ?>
