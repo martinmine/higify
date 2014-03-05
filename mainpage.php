fd3e97ad00461c638c315ded8357dba894773088
jjoereng <jjoereng@gmail.com>
2014-03-05 12:58:33 +0100

    reverted fuckup

--- mainpage.php
+++ mainpage.php
@@ -13,14 +13,9 @@ $tpl->setValue('PAGE_TITLE', 'Your Higify');
 $tpl->setValue('CSS', array('mainpage', 'search', 'menu'));
 $tpl->registerController(new MainPageController());
 $tpl->appendTemplate('MainPageCenter');
-<<<<<<< HEAD
 $tpl->appendTemplate('MainPageSideMenu');
 $tpl->registerController(new MainPageScheduleController());
-=======
-$tpl->appendTemplate('MainPageScheduleContainer');
 $tpl->registerController(new MainPageScheduleController($userID));
->>>>>>> 71c620711f5381fa53b41c545494b46d19e21465
-$tpl->appendTemplate('MainPageFooter');
 $tpl->display();
 
 ?>
