f0b3e9fab1e5668f7cb4e3803193b8d48a27d5a2
Martin <martin_mine@hotmail.com>
2014-03-07 12:45:05 +0100

    View note by category

--- NoteListCategoryController.class.php
+++ NoteListCategoryController.class.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once('Template/IPageController.interface.php');
 require_once('NoteController.class.php');
 require_once('UserController.class.php');
@@ -7,16 +6,11 @@ require_once('SessionController.class.php');
 require_once('NoteType.class.php');
 require_once('NoteListView.class.php');
 
-class ViewNoteController implements IPageController
+class NoteListCategoryController implements IPageController
 {   
     public function onDisplay()
     {        
-        $vals = array();
-        
-        $vals['ORIGINAL'] = new NoteListView(NULL, NoteType::NONE, $_GET['id']);
-        $vals['NOTES'] = new NoteListView(NULL, NoteType::REPLY, $_GET['id']);
-        
-        return $vals;
+        return array('NOTES' => new NoteListView(NULL, NoteType::NONE, NULL, $_GET['cat']));
     }
 }
 ?>
---
+++ Template/Tpl/NoteCategory.tpl
@@ -0,0 +1,6 @@
+        <div class="pageSideContainer">
+            <div class="pageElement">
+				<div class="spacer"></div>
+                <?php echo $NOTES; ?>
+            </div>
+        </div>
