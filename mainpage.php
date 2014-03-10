4e7c6ccf9a54cdbdba9563297a5796bd2a7da143
Martin <martin_mine@hotmail.com>
2014-03-10 19:38:24 +0100

    Displays color code for notes

--- mainpage.php
+++ mainpage.php
@@ -12,7 +12,7 @@ $tpl = new Template();
 $tpl->appendTemplate('MainPageHeader');
 $tpl->setValue('PAGE_TITLE', 'Mainpage');
 $tpl->setValue('BANNER_TITLE', 'Your Higify');
-$tpl->setValue('CSS', array('mainpage', 'search', 'menu', 'schedule', 'schedule_mainpage'));
+$tpl->setValue('CSS', array('mainpage', 'search', 'menu', 'schedule', 'schedule_mainpage', 'editor'));
 $tpl->setValue('JS', array('jquery-latest.min', 'menu'));
 $tpl->registerController(new BannerController());
 $tpl->registerController(new MainPageController());
--- view_note.php
+++ view_note.php
@@ -8,7 +8,7 @@ $tpl = new Template();
 $tpl->appendTemplate('MainPageHeader');
 $tpl->setValue('PAGE_TITLE', 'View Note');
 $tpl->setValue('BANNER_TITLE', 'View a Note');
-$tpl->setValue('CSS', array('mainpage', 'search', 'menu', 'schedule', 'schedule_mainpage'));
+$tpl->setValue('CSS', array('mainpage', 'search', 'menu', 'schedule', 'schedule_mainpage', 'editor'));
 $tpl->setValue('JS', array('jquery-latest.min', 'menu'));
 $tpl->registerController(new BannerController());
 $tpl->registerController(new ViewNoteController());
