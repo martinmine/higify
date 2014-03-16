6c9c2f0cedd6afeee53b050206aa60c202cd16cc
Martin <martin_mine@hotmail.com>
2014-03-16 14:44:02 +0100

    Renaming Static to static
    Because Github for Windows doesnt care about upper/lowercases, but
    sometimes it does (on committ)

--- Static/search_results.js
+++
@@ -1,9 +0,0 @@
-{
-	"count":3,
-	"results": 
-	[
-		{"id": "1", "name": "IMT2291 - WWW-teknologi"},
-		{"id": "2", "name": "IMT2282 - Operativsystemer"},
-		{"id": "3", "name": "IMT3681 - IT-ledelse"}
-	]
-}
