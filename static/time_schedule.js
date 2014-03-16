1ddabc2d1c1251de580c30459558ea51570f50c4
Martin <martin_mine@hotmail.com>
2014-03-16 14:46:50 +0100

    Moved items in Static to static

---
+++ static/time_schedule.js
@@ -0,0 +1,107 @@
+/**
+* Runs a xpath query and returns the result
+*/
+function runXpath(xpathExpression)
+{
+	var ORDERED_NODE_SNAPSHOT_TYPE = 7;
+	return document.evaluate(xpathExpression, document, null, ORDERED_NODE_SNAPSHOT_TYPE, null);
+}
+
+/**
+* Sets the height and width for all elements found with an xpath expression
+*/
+function setHeightAndWidth(xpathExpression, width, height)
+{
+	var elements = runXpath(xpathExpression);
+
+	for (var i = 0; i < elements.snapshotLength; i++)
+	{
+		var element = elements.snapshotItem(i);
+		element.style.height = height + "px";
+		element.style.width = width + "px";
+	}
+}
+
+/**
+* The width of one cell in the time schedule
+*/
+var cellWidth;
+
+/**
+* The height for one cell in the time schedule
+*/
+var cellHeight;
+
+/**
+* Sets the default width and height for one cell in the time schedule
+*/
+function setTimeObjectLayout(width, height)
+{
+	cellWidth = width;
+	cellHeight = height;
+}
+
+/**
+* Sets the properties for one time object by the object's ID
+* @offset integer Amount of minutes the object shall be moved down on the schedule
+* @duration integer The duration of the time objects in minutes
+*/
+function setTimeObject(id, offset, duration, indent, indentMax)
+{
+	var element = document.getElementById(id);
+
+	var offset = Math.floor(offset * (cellHeight / 60));
+	var length = Math.floor(duration * (cellHeight / 60));
+
+	var width = (CELL_WIDTH / indentMax) - 8;
+	var widthOffset = (indent - 1) * (CELL_WIDTH / indentMax);
+
+	if (length > cellHeight)	// If cross a border
+	{
+		length += Math.floor(length / cellHeight) * 3; // Add the size of the borders we crossed
+	}
+
+	element.style.height = length + "px";
+	element.style.width = width + "px";
+	element.style.marginLeft = widthOffset + "px";
+	element.style.marginTop = offset + "px";
+}
+
+/**
+* Sets the height for all the elements found with an xpath expression
+*/
+function setHeight(xpathExpression, height)
+{
+	var elements = runXpath(xpathExpression);
+
+	for (var i = 0; i < elements.snapshotLength; i++)
+	{
+		var element = elements.snapshotItem(i);
+		element.style.height = height + "px";
+	}
+}
+
+/**
+* Formats the time table according to the given parameters
+*/
+function formatTimeTable(cellHeight, cellWidth, cellDayHeaderHeight, cellDayPaddingHeight, cellTimeWidth)
+{
+	setContainerWidth(cellWidth, cellTimeWidth);
+	setHeightAndWidth('//div[@class="weekIdentifier"]', cellTimeWidth - 4, cellDayHeaderHeight);
+	setHeightAndWidth('//div[@class="timeIdentifier"]', cellTimeWidth - 4, cellHeight);
+	setHeightAndWidth('//div[@class="dayHeader"]', cellWidth, cellDayHeaderHeight);
+	setHeightAndWidth('//div[@class="singleHour"]', cellWidth, cellHeight);
+
+	setHeightAndWidth('//div[contains(@class, "dayPadding")]', cellWidth, cellDayPaddingHeight);
+	setHeightAndWidth('//div[contains(@class, "weekPadding")]', cellTimeWidth - 4, cellDayPaddingHeight);
+}
+
+/**
+* Sets the width of the time schedule container
+*/
+function setContainerWidth(cellWidth, celltimerWidth)
+{
+	var container = document.getElementById('scheduleContainer');
+	var totalWidth = celltimerWidth + 2 + (cellWidth + 1) * 5;
+	container.style.width = totalWidth + 'px';
+}
