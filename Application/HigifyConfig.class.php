27d073b7ff11adf4b6c0ef4c77f0c0b89e38cb98
martinmine <martin_mine@hotmail.com>
2014-04-29 21:48:44 +0200

    Reverted back to empty config file
    No, these passwords does no longer work

--- Application/HigifyConfig.class.php
+++ Application/HigifyConfig.class.php
@@ -5,32 +5,32 @@ class HigifyConfig
     /**
      * The host-name used for connecting to the databse. Can also be IP-address.
      */
-    const DB_HOST = '128.39.41.101';
+    const DB_HOST = '';
     
     /**
      * Username used when connecting to the database
      */
-    const DB_NAME = 's121050';
+    const DB_NAME = '';
     
     /**
      * Name of the database
      */
-    const DB_USER = 's121050';
+    const DB_USER = '';
     
     /**
      * Super secret database password
      */
-    const DB_PSSW = 'xSfuv1jw';
+    const DB_PSSW = '';
     
     /**
      * Private recaptcha API key
      */
-    const RECAPTCHA_PRIVATE_KEY = '6Lfis-4SAAAAANi6ne8PD0-i9U9z8X8PvKnGhJiG';
+    const RECAPTCHA_PRIVATE_KEY = '';
     
     /**
      * Public recaptcha API key
      */
-    const RECAPTCHA_PUBLIC_KEY = '6Lfis-4SAAAAAKnGTAWkuCwryfiHKK0OlNM0MjIe';
+    const RECAPTCHA_PUBLIC_KEY = '';
     
 
     public static $BAD_PASSWORDS = array ('123456', 'password' , '12345678', 'qwerty', 'abc123', '123456789', '111111', 
@@ -46,17 +46,17 @@ class HigifyConfig
     /**
      * Salt used for salting the users password
      */
-    const SALT = 'abcfds11jhG';
+    const SALT = '';
     
      /**
      * Site key used for initializing the users password
      */
-    const SITEKEY = 'n8eyr2nsdasd23119bh91b';
+    const SITEKEY = '';
     
      /**
      * 
      */
-    const MAIL_HOST = 'mail.dongsecurity.com';                // Specify main and backup server
+    const MAIL_HOST = '';                // Specify main and backup server
     
     /**
      * Use AMPTH authentication for mail
@@ -66,37 +66,37 @@ class HigifyConfig
     /**
      * The username for the mail account
      */
-    const MAIL_USERNAME = 'activation@obbahhost.com';
+    const MAIL_USERNAME = '';
     
     /**
      * Password for mail account
      */
-    const MAIL_PASSWORD = 'Vtft32XRp8FVZQGvETu4vpmM';
+    const MAIL_PASSWORD = '';
     
     /**
      * SMTP port
      */
-    const MAIL_PORT = 94;
+    const MAIL_PORT = 25;
     
     /**
      * Who the email is from
      */
-    const MAIL_TITLE_FROM = 'activation@obbahhost.com';
+    const MAIL_TITLE_FROM = '';
     
     /**
      * And the title on the from name
      */
-    const MAIL_TITLE_FROM_NAME = 'The Higify Team';
+    const MAIL_TITLE_FROM_NAME = '';
 
     /**
      * The salt used to create new tokens
      */
-    const COOKIE_SALT = 'xEvMHDfVbM6SYZN7EsBQ6NUFpT7PyGnYfSfbWFeTETb7vJ4Y2zFtBmxd6uysYpEFMvp7ZFqdPbJHPbbjFEhUPQb524ZMRpaJcy9qCS5h8aPgad9JZ4ArcqXy';
+    const COOKIE_SALT = '';
     
     /**
      * Random key used for initializing the hash function
      */
-    const HASH_INIT = 'hQVJ5TaKt7QTaScYrvFsaPPJKEpB56X6y9QzEYKcNvST8DpYDymrx7JWfGDUBHSpBugzw3L2Ce9MuAeGe6KNw2MCvMsjCnmEwtuHDgWyXHV2HVW7K3GQgT7g'; 
+    const HASH_INIT = ''; 
 }
 
 ?>
